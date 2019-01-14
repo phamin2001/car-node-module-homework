1e253e52a46cae840252c589677ebfff03ff2bdd
phamin2001 <amin.pahlavani@gmail.com>
2019-01-13 18:35:21 -0700

    commit

--- Car.js
+++ Car.js
@@ -1,5 +1,17 @@
 class Car{
-    
+    constructor(color, convertible) {
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0;
+    }
+
+    accelerate(num) {
+        this.speed += num;
+    }
+
+    decelerate(num) {
+        this.speed -= num;
+    }
 }
 
 module.exports = Car;
